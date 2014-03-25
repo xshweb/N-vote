0737ba274c00c71a2dc7ce1fd9f0bdc012bef70f
weirongxu <weirongxu.raidou@gmail.com>
2014-03-26 05:17:15 +0800

    merge 大通社 picture & text vote.

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,5 @@
 var settings = {
-  title      : "广西民族大学首届随手拍大赛 网上投票",
+  title      : "广西民族大学“梦圆中国·美在民大”摄影大赛",
   host       : 'http://localhost:3000',
   prefix     : 'pic-vote',
   port       : process.env.NODE_PORT || 3000,
